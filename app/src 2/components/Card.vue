31487e8691e0dab4b2a5a0d5561d0a9838895062
maxiiiktsu <maxim.fruhwald@gmail.com>
2023-04-12 12:46:55 +0200

    aiifh

--- app/src 2/components/Card.vue
+++ app/src 2/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
     
     <div class=" flex flex-col w-[19rem] m-4 drop-shadow-md" >
-      <img class="w-[19rem] h-[11rem] rounded-t-2xl" src="src/assets/images/mcdonald-card.png" alt="">
+      <img class="w-[19rem] h-[11rem] rounded-t-2xl" src="src/assets/images/mcdonalds.png" alt="">
       
       <div class="bg-[#F8EDE3] rounded-b-2xl">
         <h1 class="text-2xl mt-2 ml-4">McDonalds</h1>
@@ -14,7 +14,7 @@
           <img src="src/assets/images/rating-star.png" alt="">
         </div>
       </div>
-
+      
     </div>
 
 
