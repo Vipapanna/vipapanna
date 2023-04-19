eae641752f9042e2ee8032b588030c99faf9f410
maxiiiktsu <maxim.fruhwald@gmail.com>
2023-04-19 14:42:01 +0200

    ck

--- app/src/components/HomePage.vue
+++ app/src/components/HomePage.vue
@@ -185,15 +185,19 @@
   
     </section>
   
-    <div class="flex bg-[#4C4556] w-full">
-      <div class="bg-[#4C4556] text-white grid grid-cols-2  items-center justify-center w-[30rem] font-sans" >
-        <a class=" text-xl" href="">About Us</a>
-        <a class=" text-xl" href="">Cookies</a>
-        <a class=" text-xl" href="">FAQs</a>
-        <a class=" text-xl" href="">Contact Us</a>
-  
+    <div class="flex bg-[#4C4556] w-full items-center justify-center h-44 ">
+      <div class="bg-[#4C4556] text-white grid grid-cols-2  items-center justify-center  w-60 text-3xl  gap-10" >
+        <a  style="font-family: 'Cabin', sans-serif;" class=" text-xl" href="">About Us</a>
+        <a  style="font-family: 'Cabin', sans-serif;" class=" text-xl" href="">Cookies</a>
+        <a  style="font-family: 'Cabin', sans-serif;" class=" text-xl" href="">FAQs</a>
+        <a  style="font-family: 'Cabin', sans-serif;" class=" text-xl" href="">Contact Us</a>
       </div>
-      <div>iconka</div>
+  
+      <div class=" flex flex-row  space-x-12" >
+        <object data="./src/assets/images/fb-logo.svg" type="image/svg+xml"></object>
+        <object data="./src/assets/images/ig-logo.svg" type="image/svg+xml"></object>
+        <object data="./src/assets/images/tw-logo.svg" type="image/svg+xml"></object>
+      </div>
     </div>
 
     
@@ -291,4 +295,11 @@
     }
   }
   </script>
+
+  <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Cabin&display=swap');
+
+
+
+</style>
   
