12598f39d70d8028e3fdc0e5f80427476b078950
Michal <michal.rabik33@gmail.com>
2023-05-09 08:26:08 +0200

    rest page fixed
    som šéf fakt more čo ti jebe

--- app/src/store.js
+++ app/src/store.js
@@ -5,36 +5,49 @@ const store = createStore({
   state: {
     cards: [],
     selectedCard: null,
-},
-mutations: {
+  },
+  mutations: {
     setCards(state, cards) {
       state.cards = cards;
     },
     setSelectedCard(state, card) {
-        state.selectedCard = card
+      state.selectedCard = card;
+      localStorage.setItem('selectedCard', JSON.stringify(card));
+    },
+    retrieveSelectedCard(state) {
+      const card = JSON.parse(localStorage.getItem('selectedCard'));
+      if (card) {
+        state.selectedCard = card;
+      }
+    },
   },
-},
-actions: {
+  actions: {
     fetchCards(context) {
       axios
         .get('https://vypapanna.hybridlab.dev/cms/api/v1/restaurants')
         .then(response => {
-          if (Array.isArray(response.data.data)) {
-            const cards = response.data.data.map(item => ({
-              title: item.restaurant_name,
-              rating: item.review,
-              star: Math.floor(item.review)
-            }))
-            context.commit('setCards', cards)
-          } else {
-            console.error('Error fetching cards')
-          }
+          const cards = response.data.data.map(item => ({
+            title: item.restaurant_name,
+            rating: item.review,
+            star: Math.floor(item.review)
+          }))
+          context.commit('setCards', cards)
         })
         .catch(error => {
           console.error(error)
         })
-    }
-  }
-})
+    },
+    retrieveSelectedCard(context) {
+      context.commit('retrieveSelectedCard');
+    },
+  },
+  getters: {
+    getSelectedCard: state => {
+      return state.selectedCard;
+    },
+  },
+});
 
-export default store
+store.dispatch('retrieveSelectedCard');
+
+export default store;
