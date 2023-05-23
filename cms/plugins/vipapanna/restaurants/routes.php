0fd03db67d1fa487d5e8896ba85bdf068c14586d
Alexandra Bartolenová <alexandrabartolenova@gmail.com>
2023-05-23 10:34:05 +0200

    search

--- cms/plugins/vipapanna/restaurants/routes.php
+++ cms/plugins/vipapanna/restaurants/routes.php
@@ -6,11 +6,10 @@ use Vipapanna\Restaurants\Http\Controllers\RestaurantController;
 use Vipapanna\Restaurants\Http\Controllers\FoodController;
 
 Route::group(['prefix' => 'api/v1'], function () {
-
+    Route::get('restaurant/search', [RestaurantController::class, 'restaurantSearch']);
     Route::get('restaurant/{id}', [RestaurantController::class, 'restaurantID']);
     Route::get('restaurants', [RestaurantController::class, 'restaurant']);
     Route::get('food/{food_id}', [FoodController::class, 'food']);
     Route::get('restaurant/food/{restaurant_id}', [FoodController::class, 'restaurantFoods']);
-    Route::get('restaurant/search}', [RestaurantController::class, 'restaurantSearch']);
 });
 
