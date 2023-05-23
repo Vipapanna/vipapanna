ef210be28a821a760527f15483eaa1a0b7a6e977
maxiiiktsu <maxim.fruhwald@gmail.com>
2023-05-23 08:09:07 +0200

    dhe

--- app/src/components/Card.vue
+++ app/src/components/Card.vue
@@ -1,39 +1,42 @@
 <template>
-    <div class=" flex flex-col w-[19rem] m-4 drop-shadow-md hover:drop-shadow-xl">
-      <img class="w-[19rem] h-[11rem] rounded-t-2xl" :src="image" alt="">
-      
-      <div class="bg-[#F8EDE3] rounded-b-2xl">
-        <h1 class="text-2xl mt-2 ml-4">{{ title }}</h1>
+  <div class="flex flex-col w-[21rem] m-4 drop-shadow-md hover:drop-shadow-xl">
+    <img class="w-[21rem] h-[11rem] rounded-t-2xl" :src="image" alt="" />
 
-        <div class="flex flex-row ml-3 mb-2  ">
-          <img v-for="index in star" :key="index" src="/src/assets/images/rating-star.svg" alt="">
-          <a> {{ rating }}</a>
+    <div class="bg-[#F8EDE3] rounded-b-2xl">
+      <h1 class="text-2xl mt-2 ml-4">{{ title }}</h1>
 
-        </div>
+      <div class="flex flex-row ml-3 mb-2">
+        <img
+          v-for="index in star"
+          :key="index"
+          src="/src/assets/images/rating-star.svg"
+          alt=""
+        />
+        <a> {{ rating }}</a>
       </div>
-
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  props:{
-    title:{
+  props: {
+    title: {
       require: true,
       type: String,
     },
-    image:{
+    image: {
       require: true,
-      type: String
+      type: String,
     },
-    rating:{
+    rating: {
       require: true,
-      type: Number
+      type: Number,
     },
-    star:{
+    star: {
       require: true,
-      type: Number
-    }
-  }
-}
+      type: Number,
+    },
+  },
+};
 </script>
